6105c4b0b6d6f6e54e9290b527a3cc5aabbc1f2a
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-03-25 14:36:25 -0400

    creating list

--- react_practice/src/Home.js
+++ react_practice/src/Home.js
@@ -1,17 +1,22 @@
-
+import { useState } from 'react'
 
 const Home = () => {
 
-    const handleClick = () => {
-        console.log("Hello World")
-    }
+    const [blogs, setBlogs] = useState([
+        {title:'My typing speed', body:'is 85wpm', author:'ZAHIN', id:1},
+        {title:'Practice', body:'makes perfect', author:'Fariya', id:2},
+        {title:'I need to stop', body:'using your name', author:'death', id:3}
 
+    ]);
 
     return ( 
         <div className="home">
-            <h2>HOME PAGE</h2>
-            <button onClick={handleClick}>Click Me</button>
-            <button>Click ME again</button>
+          {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </div>
+          ))}
         </div>
      );
 }
